60dd9f9b817a7769925de05c4e01bb5cb7ca4ec5
Kate <49902595+katebrighteyes@users.noreply.github.com>
2021-11-05 15:34:33 +0900

    Update opencvtest.py

--- libtest/opencvtest.py
+++ libtest/opencvtest.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 
 # Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+# TODO 1
+
 
 # Get device product line for setting a supporting resolution
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
@@ -29,15 +29,16 @@ else:
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
-pipeline.start(config)
+#TODO 2
+
 
 try:
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+        # TODO 3
+
         if not depth_frame or not color_frame:
             continue
 
