4342bf5b4a394c76b282a283b2c9a78f3332a1ad
Kate <49902595+katebrighteyes@users.noreply.github.com>
2021-11-05 15:31:48 +0900

    Update depthtest.py

--- libtest/depthtest.py
+++ libtest/depthtest.py
@@ -8,14 +8,17 @@ try:
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-    # Start streaming
-    pipeline.start(config)
+    # TODO 1 Start streaming
+
 
     while True:
         # This call waits until a new coherent set of frames is available on a device
         # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
         frames = pipeline.wait_for_frames()
-        depth = frames.get_depth_frame()
+        
+        # TODO2 get depth
+        
+        
         if not depth: continue
 
         # Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and approximating the coverage of pixels within one meter
